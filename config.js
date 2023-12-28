b9300e375f6e1d1533d8653780bd11525acb6b88
baramsivaramireddy <baramsivarami@gmail.com>
2023-12-28 23:51:59 +0530

    remove local

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports ={
     MONGO_URI:process.env.MONGO_URI ,
-    ENVIRONMENT:process.env.ENV_TYPE|| 'local',
+    ENVIRONMENT:process.env.ENV_TYPE,
     SECRET_KEY:process.env.SECRET_KEY,
     SALTROUND :10
 }
